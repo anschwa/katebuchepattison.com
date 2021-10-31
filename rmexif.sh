4dbc6edf0badf69d82025d79debcc173de1123a5
Adam Schwartz <adam@anschwa.com>
2021-10-31 21:45:23 +0100

    add copenhagen post

--- rmexif.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-# Strip all EXIF data from images in a directory
-# WARNING this will overwrite the original files
-
-exiftool -overwrite_original -all:all= -r "$1"
